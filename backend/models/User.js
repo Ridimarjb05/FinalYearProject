43a706d99ddb8637943bce92b7a377d10f8d2068
Ridimarjb05 <ridimarjb2005@gmail.com>
2026-03-01 10:00:00 +0545

    Add User model

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    businessName: {
+        type: String,
+        required: true,
+    },
+    pan: {
+        type: String,
+        default: '',
+    },
+    address: {
+        type: String,
+        default: '',
+    }
+});
+
+const UserModel = mongoose.model('users', UserSchema);
+module.exports = UserModel;
